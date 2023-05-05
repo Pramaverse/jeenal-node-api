ac872c7aa0cc86fa939fc96e3a8f45da0219276f
jeenalpatel <jspatel1993@gmail.com>
2023-05-05 00:38:59 -0700

    modified test files

--- src/handlers/cart.spec.ts
+++ src/handlers/cart.spec.ts
@@ -16,7 +16,7 @@ import { CartModel } from "../models/cart";
 import { Application } from "express";
 import mongoose from "mongoose";
 
-describe("/cart", () => {
+describe("Cart API Endpoints", () => {
   let app: Application;
   let userData: any;
   let token: string;
@@ -40,55 +40,68 @@ describe("/cart", () => {
     await CartModel.deleteMany({});
     await mongoose.connection.close();
   });
-  it("should respond 201 after adding product to cart", async () => {
-    const response = await request(app)
-      .post(`/cart/products/${product.id}`)
-      .set("Authorization", `Bearer ${token}`)
-      .send({ productId: product.id, quantity: 1 })
-      .expect(201);
-    expect(response.body.message).toBe("Product added to cart successfully");
-  });
-  it("should respond 400 if we miss to pass productId|quantity", async () => {
-    const response = await request(app)
-      .post(`/cart/products/${product.id}`)
-      .set("Authorization", `Bearer ${token}`)
-      .send({})
-      .expect(400);
-    expect(response.body.message).toBe("invalid input");
-  });
-  it("should respond 200 once we update the quantity of product in cart", async () => {
-    const response = await request(app)
-      .put(`/cart/products/${product.id}`)
-      .set("Authorization", `Bearer ${token}`)
-      .send({ productId: product.id, quantity: 2 })
-      .expect(200);
-    expect(response.body.message).toBe("Cart updated successfully");
-  });
-  it("should respond 400 if we provide wrong productId", async () => {
-    const response = await request(app)
-      .put(`/cart/products/${product.id}123`)
-      .set("Authorization", `Bearer ${token}`)
-      .send({ quantity: 2 })
-      .expect(400);
-    expect(response.body.message).toBe("invalid input");
-  });
-  it("should respond 200 once we delete the product from cart", async () => {
-    const response = await request(app)
-      .delete(`/cart/products/${product.id}`)
-      .set("Authorization", `Bearer ${token}`)
-      .send({ productId: product.id })
-      .expect(200);
-    expect(response.body.message).toBe(
-      "Product deleted from cart successfully"
-    );
-  });
-  it("should respond with an array of products in cart", async () => {
-    await request(app)
-      .get("/cart/products")
-      .set("Authorization", `Bearer ${token}`)
-      .expect(200)
-      .then((response) => {
-        expect(response.body.length).toBeGreaterThan(0);
+  describe("User Accessible Endpoints", () => {
+    describe("GET /cart/products", () => {
+
+      it("should respond with an array of products in cart", async () => {
+        await CartModel.create({productId: product.id, userId: userData.id, quantity: 1});
+        await request(app)
+            .get("/cart/products")
+            .set("Authorization", `Bearer ${token}`)
+            .expect(200)
+            .then((response) => {
+              expect(response.body.length).toBeGreaterThan(0);
+            });
       });
+    });
+    describe("POST /cart/products/:productId", () => {
+      it("should respond 201 after adding product to cart", async () => {
+        const response = await request(app)
+            .post(`/cart/products/${product.id}`)
+            .set("Authorization", `Bearer ${token}`)
+            .send({ productId: product.id, quantity: 1 })
+            .expect(201);
+        expect(response.body.message).toBe("Product added to cart successfully");
+      });
+      it("should respond 400 if we miss to pass productId|quantity", async () => {
+        const response = await request(app)
+            .post(`/cart/products/${product.id}`)
+            .set("Authorization", `Bearer ${token}`)
+            .send({})
+            .expect(400);
+        expect(response.body.message).toBe("invalid input");
+      });
+    });
+    describe("PUT /cart/products/:productId", () => {
+      it("should respond 200 once we update the quantity of product in cart", async () => {
+        const response = await request(app)
+            .put(`/cart/products/${product.id}`)
+            .set("Authorization", `Bearer ${token}`)
+            .send({ productId: product.id, quantity: 2 })
+            .expect(200);
+        expect(response.body.message).toBe("Cart updated successfully");
+      });
+      it("should respond 400 if we provide wrong productId", async () => {
+        const response = await request(app)
+            .put(`/cart/products/${product.id}123`)
+            .set("Authorization", `Bearer ${token}`)
+            .send({ quantity: 2 })
+            .expect(400);
+        expect(response.body.message).toBe("invalid input");
+      });
+    });
+    describe("DELETE /cart/products/:productId", () => {
+      it("should respond 200 once we delete the product from cart", async () => {
+        const response = await request(app)
+            .delete(`/cart/products/${product.id}`)
+            .set("Authorization", `Bearer ${token}`)
+            .send({ productId: product.id })
+            .expect(200);
+        expect(response.body.message).toBe(
+            "Product deleted from cart successfully"
+        );
+      });
+    });
   });
+
 });
